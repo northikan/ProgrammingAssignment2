4188b187e7a2c10790fc1f1a7c62fdadbadd697f
northikan <nori.praveen@gmail.com>
2018-07-23 23:08:49 -0700

    + comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,18 @@
 
 makeCacheMatrix <- function(m = matrix()) {
         im <- NULL
+        #function to Set matrix into the environment
         setM <- function(y) {
             m <<- y
             im <<- NULL
         }
+        #function to get the matrix from the environment
         getM <- function() m
+        #function to set the Inverse of the matrix into the environemnt
         setI <- function(inverseM) im <<- inverseM
+        #function to get the Inverse of the matrix from the environment
         getI <- function() im
+        #List of functions to manage matrix and its inverse in the environemnt
         list(set = setM, get = getM,
              setinverse = setI,
              getinverse = getI)
@@ -24,11 +29,16 @@ makeCacheMatrix <- function(m = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is managed by the MakeCacheMatrix list x
+    # try to get the inverse of the matrix from environment
     im <- x$getinverse()
+    # Does inverse exists. if so, return the inverse
     if(!is.null(im)) {
         message("getting cached Inverse Matrix..")
         return(im)
     }
+    # If Inverse does not exist, get the Matrix, calculate inverse and 
+    # set inverse into environment
+    # return inverse
     matrix <- x$get()
     im <- solve(matrix, ...)
     x$setinverse(im)
